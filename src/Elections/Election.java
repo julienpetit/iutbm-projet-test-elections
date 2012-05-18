07352918227827f23bb03a8857fb412784c89f02
patrocle <julien.petit.mathay@gmail.com>
2012-05-18 14:43:50 +0200

    * Correction de l'initialisation de la MAP resultat

--- src/Elections/Election.java
+++ src/Elections/Election.java
@@ -52,5 +52,4 @@ public class Election {
         }
         return candidats;
     }
-
 }
---
+++ test/Elections/ElectionTest.java
@@ -0,0 +1,12 @@
+package Elections;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: julienpetit
+ * Date: 5/17/12
+ * Time: 11:46 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ElectionTest {
+
+}
