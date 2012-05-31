65c0c37a9b6be0849f99b68c2d7b95eb6dbc6ef5
Maxime <bolschack@gmail.com>
2012-05-31 08:33:11 +0200

    Candidat Test

--- test/Elections/GrilleResultatTest.java
+++ test/Elections/GrilleResultatTest.java
@@ -15,20 +15,48 @@ public class GrilleResultatTest{
 
     @Test
     public void GrilleResultatTest(){
-        Candidat candidat1 = new Candidat("Hollande");
-        Candidat candidat2 = new Candidat("Sarkozy");
-        Candidat candidat3 = new Candidat("LePen");
+        Candidat c1 = new Candidat("Hollande");
+        Candidat c2 = new Candidat("Sarkozy");
+        Candidat c3 = new Candidat("LePen");
+
         Set<Candidat> candidats = new HashSet<Candidat>();
-        candidats.add(candidat1);
-        candidats.add(candidat2);
-        candidats.add(candidat3);
+
+        candidats.add(c1);
+        candidats.add(c2);
+        candidats.add(c3);
 
         GrilleResultat grilleResultat = new GrilleResultat(candidats);
 
+        Bulletin b1 = new Bulletin();
+        Bulletin b2 = new Bulletin();
+        Bulletin b3 = new Bulletin();
+        Bulletin b4 = new Bulletin();
+        Bulletin b5 = new Bulletin();
+
+        b1.addCandidat(c1);
+        b2.addCandidat(c2);
+        b2.addCandidat(c3);
+        b3.addCandidat(c3);
+        b4.addCandidat(c2);
+        //b4.addCandidat(c1);
+        b5.addCandidat(c2);
+
         List<Bulletin> bulletins = new ArrayList<Bulletin>();
 
+        bulletins.add(b1);
+        bulletins.add(b2);
+        bulletins.add(b3);
+        bulletins.add(b4);
+        bulletins.add(b5);
 
+        System.out.println(b1.getCandidats());
+        System.out.println(b2.getCandidats());
+        System.out.println(b3.getCandidats());
+        System.out.println(b4.getCandidats());
+        System.out.println(b5.getCandidats());
 
+        grilleResultat.depouillement(bulletins);
+        System.out.println(grilleResultat.prepareAnnonce());
 
     }
 }
